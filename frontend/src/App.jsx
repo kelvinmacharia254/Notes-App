ace7527426610c73ab90590d015abaf2a4b3109b
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-10-23 09:57:12 +0300

    feat: frontend - create a react frontend using vite - add routes with dummy text for testing purposes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import {createBrowserRouter, RouterProvider, Navigate} from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Register from "./pages/Register.jsx";
+
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+function logout(){
+    // logout by removing the access token from the local storage
+    localStorage.removeItem("access");
+    return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+    // Remove the access token from the local storage before registering a new user
+    localStorage.removeItem("access");
+    return <Register />
+}
 
 function App() {
-
-
+    const router = createBrowserRouter([
+        {path: "/", element: <ProtectedRoute><Home /></ProtectedRoute>},
+        {path: "/login", element: <Login />},
+        {path: "/register", element: <RegisterAndLogout />},
+        {path: "*", element: <NotFound/>},
+    ]);
   return (
-    <>
-
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
